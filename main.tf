8ef50ff9dca53db6709f62effcaa1e9404196ae2
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2024-07-01 23:29:13 +0900

    2.4 Webサーバ1台のデプロイ

--- main.tf
+++ main.tf
@@ -5,8 +5,31 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami = "ami-0fb653ca2d3203ac1"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    # 最初のインスタンス起動時にのみ実行されるスクリプト
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello, World" > index.html
+        nohup busybox httpd -f -p 8080 &
+    EOF
+
+    # ユーザーデータの変更を適用するために，毎回インスタンスをリプレイスする
+    user_data_replace_on_change = true
 
     tags = {
         Name = "terraform-example"
+    }
+}
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+
+    # Webサーバへのアクセスを許可
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] # アクセス可能なIPアドレス範囲
     }
 }
