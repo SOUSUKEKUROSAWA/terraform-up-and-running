7bad770b9701b14ef16b98e7e80b8984a953f647
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2024-07-01 21:56:11 +0900

    インデント幅を修正

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "us-east-2"
+    region = "us-east-2"
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-0fb653ca2d3203ac1"
-  instance_type = "t2.micro"
+    ami = "ami-0fb653ca2d3203ac1"
+    instance_type = "t2.micro"
 
-  tags = {
-    Name = "terraform-example"
-  }
+    tags = {
+        Name = "terraform-example"
+    }
 }
