5654fc3696211a13d7d1c9a463c8955b6bf564ce
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2024-07-16 22:57:09 +0900

    モジュール内の全てのリソースにタグを適用する方法

--- prod/services/web-server-cluster/provider.tf
+++ prod/services/web-server-cluster/provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
     region = "us-east-2"
+
+    # モジュール内で作成するすべてのリソースにデフォルトで適用するタグ
+    # -- リソースごとにこのタグを上書きすることは許容される
+    default_tags {
+        tags = {
+            ManagedBy = "Terraform"
+        }
+    }
 }
