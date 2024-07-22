beb8008abdba2b9c9990fa5bf1ce0d3e96261895
SOUSUKEKUROSAWA <kurosocavs0714@gmail.com>
2024-07-22 22:55:44 +0900

    6.3.2.3 シークレットストア

---
+++ prod/data-stores/mysql/locals.tf
@@ -0,0 +1,5 @@
+locals {
+    db_credentials = jsondecode(
+        data.aws_secretsmanager_secret_version.db_credentials.secret_string
+    )
+}
--- stage/data-stores/mysql/main.tf
+++ stage/data-stores/mysql/main.tf
@@ -5,6 +5,6 @@ resource "aws_db_instance" "example" {
     instance_class = "db.t3.micro" # 2024/06よりdb.t2はサポートされなくなったためt3を選択
     skip_final_snapshot = true
     db_name = "example_database"
-    username = var.db_username
-    password = var.db_password
+    username = local.db_credentials.username
+    password = local.db_credentials.password
 }
